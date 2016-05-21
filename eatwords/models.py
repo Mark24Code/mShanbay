bdee744130c39660c9f46ba092bdc3688c418176
Mark24 <mark.zhangyoung@qq.com>
2016-05-21 12:45:40 +0800

    ÁPI-GET words

--- eatwords/models.py
+++ eatwords/models.py
@@ -6,8 +6,9 @@ class EatwordsConfig(models.Model):
     user_id = models.CharField(default=0,max_length=100)#用户id
     book_id = models.CharField(default=0,max_length=100)#单词书
     count_id = models.CharField(default=0,max_length=100)#每日个数
-    progress = models.CharField(default=0,max_length=100)#用户进度 {book:book_id,progress:progress_id}
-    ok_id = models.CharField(default=0,max_length=100)#用户完成书列表 [1,2,3]
+    progress = models.CharField(default="",max_length=100)#用户进度 {book:book_id,progress:progress_id}
+    ok_id = models.CharField(default="",max_length=100)#用户完成书列表 [1,2,3]
+    collect = models.TextField(blank = True, null = True)  # 收藏单词列表 [1,2,3]
     db_ids = models.TextField(blank = True, null = True)#用户当前书单词ids集合(用于分析用户数据)
     created_at = models.DateTimeField(auto_now_add=True, null=True)#创建时间
 
@@ -23,6 +24,8 @@ class WordNote(models.Model):
     user_id = models.CharField(default=0,max_length=100)#用户id
     word_id = models.CharField(default=0,max_length=100)#单词id [1,2,3]
     note_content = models.TextField(blank = True, null = True)#用户单词笔记
+    is_shared = models.BooleanField(default=False)#分享
+    is_used = models.BooleanField(default=True)#逻辑删除
     created_at = models.DateTimeField(auto_now_add=True, null=True)  # 创建时间
 
 
--- eatwords/views.py
+++ eatwords/views.py
@@ -16,17 +16,17 @@ def eating(request):
         user_id = str(request.user.id)
         bookId = request.POST.get('bookId','')
         countId = request.POST.get('countId','')
-        book_name = ID2BOOKNAME(bookId)
-        book_type = BOOKNAME2TYPE(book_name)
+        book_name = ID2BOOKNAME[bookId]
+        book_type = BOOKNAME2TYPE[book_name]
 
         filter_dict = {}
         filter_dict[book_type] = True
         try:
             words_data = Words.objects.filter(**filter_dict).order_by('id')
-            words_ids = [str(word.id) for word in words_data]
-            db_ids = ",".join(words_ids)
+            db_words_ids = [str(word.id) for word in words_data]
+            db_ids = ",".join(db_words_ids)
 
-            user_config = EatwordsConfig(
+            EatwordsConfig.objects.create(
                 user_id=user_id,
                 book_id=bookId,
                 count_id=countId,
@@ -41,5 +41,86 @@ def eating(request):
         resp = jsonresponse.creat_response(200)
         resp.data = data
         return resp.get_response()
+    elif request.GET.get('_method')=='get':
+        #基础
+        user_id = str(request.user.id)
+        user_config  = EatwordsConfig.objects.filter(user_id=user_id)[-1]#这里要处理一下边界
+
+        count_id = user_config.count_id
+        count = ID2COUNT[count_id]
+        progress = user_config.progress
+
+        collect = user_config.collect
+        collect_words_ids = collect.split(',') if collect else []
+
+        db_ids = user_config.db_ids
+        db_words_ids = db_ids.split(',') if db_ids else []
+
+        if progress:
+            index = int(db_words_ids.index(progress))
+        else:
+            index = 0
+        end = index+int(count)
+        today_words_ids = db_words_ids[index:end] if db_words_ids else []
+        cur_collect_words_ids = collect_words_ids and today_words_ids
+        #Words DB Data
+        id2words = {}
+        all_words = Words.objects.all()
+        for word in all_words:
+            id2words[str(word.id)] = word.word
+
+        today_words = all_words.filter(id__in=today_words_ids)
+
+        word_id2synoym_ids = {}
+        for word in today_words:
+            if word.synonym:
+                word_id2synoym_ids[str(word.id)] = word.synonym.split(',')
+            else:
+                word_id2synoym_ids[str(word.id)] = None
+        
+        word_id2synoym = {}
+        for word_id in word_id2synoym_ids:
+            synoym_ids = word_id2synoym_ids[word_id]
+            if synoym_ids:
+                for synoym_id in synoym_ids:
+                    if word_id in word_id2synoym:
+                        word_id2synoym[word_id].append(id2words[synoym_id])
+                    else:
+                        word_id2synoym[word_id] = [id2words[synoym_id]]
+            else:
+                word_id2synoym[word_id] = None
+
+        #组织data
+        items = []
+        for word in today_words:
+            word_id = str(word.id)
+            items.append({
+                'id':word_id,
+                'meaning':word.meaning,
+                'is_collect':True if word_id in cur_collect_words_ids else False,
+                'synonym':word_id2synoym[word_id]
+            })
+
+        resp = jsonresponse.creat_response(200)
+        data = {
+            'items':items,
+        }
+        resp.data = data
+        return resp.get_response()
     else:
         return render_to_response('eating.html',{})
+
+# #Note DB Data
+# share_notes = WordNote.objects.filter(word_id__in=today_words_ids,is_shared=True,is_used=True)
+# note_ids = [str(note.id) for note in share_notes]
+# word_id2notes = {}
+# for note in share_notes:
+#     note_dict = {}
+#     note_dict['user_id'] = note.user_id
+#     note_dict['word_id'] = note.word_id
+#     note_dict['note_content'] = note.note_content
+#
+#     if note.user_id in word_id2notes:
+#         word_id2notes[note.user_id].append(note_dict)
+#     else:
+#         word_id2notes[note.user_id] = [note_dict]
